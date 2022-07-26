dabf4bb1f553c17b36682a04741c784b6f2e740e
pbouget <philippe.bouget@laposte.net>
2022-07-26 15:38:45 +0200

    maj js et css

--- src/main/resources/static/js/pilotes.js
+++ src/main/resources/static/js/pilotes.js
@@ -1,6 +1,9 @@
 // Juste remplir le tableau html avec la liste des pilotes
 $(document).ready( function () {
 	 var table = $('#tablePilotes').DataTable({
+	 		"language": {
+            				url: 'french/fr-FR.json'
+        	},
 			"sAjaxSource": "/api/pilotes",
 			"sAjaxDataProp": "",
 			"order": [[ 0, "asc" ]],
